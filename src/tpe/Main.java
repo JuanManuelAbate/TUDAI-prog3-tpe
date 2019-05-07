153639a4e72f7a2e1e01bd74aef6ac96ce91c9e5
Juan Manuel Abate <juan.abate@gmail.com>
2019-05-07 15:57:18 -0300

    Creacion de menu para puntos 1, 2 y 3

--- src/tpe/Main.java
+++ src/tpe/Main.java
@@ -1,6 +1,7 @@
 package tpe;
 
 import java.util.Iterator;
+import java.util.Scanner;
 
 import dominio.*;
 import respuestas.VueloDirecto;
@@ -9,27 +10,82 @@ import utils.CSVReader;
 public class Main {
 	
 	public static void main(String[] args) {
-		//inicializacion 
+		//inicializacion del sistema.
 		SistemaAero sistemaAero = CSVReader.inicializarSistemaAero();
 		
 		//menu
+		boolean ejecucion = true;
+		Scanner scanner = new Scanner(System.in);
+	    int opcion;
+	    Iterator<Object> it;
 		
-		Iterator<Object> it = sistemaAero.getAeropuertos().iterator();
+		while(ejecucion) {
+			System.out.println("*******************************MENU*******************************");
+			System.out.println("Ingerese un numero de las siguientes opciones y presione enter:");
+			System.out.println("1. Listar todos los aeropuertos.");
+			System.out.println("2. Listar todas las reservas realizadas.");
+			System.out.println("3. Servicio 1: Verificar vuelo directo.");
+			System.out.println("4. Servicio 2: Obtener vuelos sin aerolinea.");
+			System.out.println("5. Servicio 3: Vuelos disponibles.");
+			System.out.println("6. Salir.");
+			System.out.println("******************************************************************");
+			opcion = scanner.nextInt();
+			scanner.nextLine();
 
-		while(it.hasNext()) {
-			Aeropuerto a=(Aeropuerto)it.next();
-			System.out.println(a.getNombre());
+		    switch (opcion) {
+		        case 1:
+		            System.out.println();
+		            System.out.println("Lista de aeropuertos:");
+		            it = sistemaAero.getAeropuertos().iterator();
+		    		while(it.hasNext()) {
+		    			Aeropuerto aeropuerto = (Aeropuerto)it.next();
+		    			System.out.println(aeropuerto.getNombre());
+		    		}
+		    		System.out.println();
+		    		break;
+		        case 2:
+		        	System.out.println();
+		            System.out.println("Lista de reservas:");
+		        	it = sistemaAero.getReservas().iterator();
+		    		while (it.hasNext()) {
+		    			Reserva reserva = (Reserva) it.next();
+		    			System.out.println("Origen:"+reserva.getOrigen()+" | Destino:"+reserva.getDestino()+" | Aerolinea:"+ reserva.getAerolinea()+" | Asientos:"+reserva.getAsientos());
+		    		}
+		    		System.out.println();
+		            break;
+		        case 3:
+		    		System.out.println();
+		    		System.out.println("Ingresar aeropuerto de origen");
+		    		String aeropuertoOrigen = scanner.nextLine();
+		    		System.out.println("Ingresar aeropuerto de destino");
+		    		String aeropuertoDestino = scanner.nextLine();
+		    		System.out.println("Ingresar aerolinea deseada");
+		    		String aerolinea = scanner.nextLine();
+		    		
+		    		VueloDirecto vueloDirecto = sistemaAero.VerificarVueloDirecto(aeropuertoOrigen, aeropuertoDestino, aerolinea);
+	    			System.out.println("Resultado:");
+		    		if (vueloDirecto == null) {
+		    			System.out.println("No existe vuelo directo para la combinacion ingresada");
+		    		} else {
+			    		System.out.println("Km requeridos:" + vueloDirecto.getKilometros() + " | Asientos disponibles:" + vueloDirecto.getCantidadAsientos());
+		    		}
+		    		System.out.println();
+		    		break;
+		        case 4:
+		            System.out.println(opcion);
+		            break;
+		        case 5:
+		        	System.out.println(opcion);
+		            break;
+		        default:
+		        	ejecucion = false;
+		    }
 		}
 		
-		it=sistemaAero.getReservas().iterator();
-		while (it.hasNext()) {
-			Reserva a= (Reserva)it.next();
-			System.out.println("Origen: "+a.getOrigen()+" Destino: "+a.getDestino()+" Aerolinea: "+ a.getAerolinea()+" Asientos: "+a.getAsientos());
-		}
-		
+		/*
 		VueloDirecto v=sistemaAero.VerificarVueloDirecto("Comodoro Benitez", "El prat", "Delta");
 		System.out.println(v.getCantidadAsientos());
 		System.out.println(v.getKilometros());
-		
+		*/
 	}
 }
